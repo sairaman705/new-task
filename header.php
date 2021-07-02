6361415304eed0a4de970f5f7a6400298079b29c
sairaman705 <79627324+sairaman705@users.noreply.github.com>
2021-07-02 13:24:01 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,57 @@
+<header> 
+    
+        <div class="row">
+            <div class="col-md-12 text-right mb-3">
+            <a href="#" class="btn btn-light btn-lg"><i class="fa fa-cloud-download"></i></a> 
+            <a href="#" class="btn btn-light btn-lg"><i class="fa fa-address-card"></i></a> 
+            <a href="#" class="btn btn-light btn-lg"><i class="fa fa-bell"></i></a> 
+            <a href="#" class="btn btn-light btn-lg"><i class="fa fa-user-circle fa-lg"></i></a> <span class="text-capitalize font-weight-bold">temporary</span>
+            <hr class="bg-light">  
+            <!--navbar starts--> 
+                <nav class="navbar navbar-expand-lg bg-white py-4"> 
+                    <div class="col-md-4 text-left">
+                        <h3 class="navbar-brand">Clients</h3>
+                    </div> 
+                    <div class="col-md-4">
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2 w-50" type="search" placeholder="Search bycustomer name/email" aria-label="Search">
+                            <button class="btn btn-primary btn-lg my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div> 
+                    <div class="col-md-4 text-center">
+                        <a class="btn btn-primary btn-lg text-capitalize p-2" href="#">add new customer</a>
+                    </div>
+                </nav>
+            <!--navbar ends--> 
+        </div> 
+
+                <!--table starts
+        <div class="row w-100">
+            <div class="col-md-12 bg-white">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                        <th scope="col">CLIENT ID</th>
+                        <th scope="col">CLOSING AGENT</th>
+                        <th scope="col">COMPANY</th>
+                        <th scope="col">ORDER</th> 
+                        <th scope="col">INVOICE PAID</th>
+                        <th scope="col">INVOICE PENDING</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row" class="text-primary">110</th>
+                        <td class="text-secondary">Ashley Bolden</td>
+                        <td class="text-secondary">Luxury Brand Pros</td>
+                        <td class="text-secondary">4</td> 
+                        <td class="text-secondary">$0</td>
+                        <td class="text-secondary">0 <span><i class="fa fa-address-card"></i></span></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>   
+            table ends-->
+     
+</header>
---
+++ index.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <section class="bg-light mb-5">
+        <div class="container-fluid bg-light p-4">
+            <?php include('header.php'); ?> 
+            <div class="row mt-3">
+                <div class="col-md-12 bg-white">
+                    <!--table starts-->
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                    <th scope="col">CLIENT ID</th>
+                                    <th scope="col">CLOSING AGENT</th>
+                                    <th scope="col">COMPANY</th>
+                                    <th scope="col">ORDER</th> 
+                                    <th scope="col">INVOICE PAID</th>
+                                    <th scope="col">INVOICE PENDING</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">110</th>
+                                    <td class="text-secondary">Ashley Bolden</td>
+                                    <td class="text-secondary">Luxury Brand Pros</td>
+                                    <td class="text-secondary">4</td> 
+                                    <td class="text-secondary">$0</td>
+                                    <td class="text-secondary">$0 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody> 
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">113</th>
+                                    <td class="text-secondary">sahana sanz</td>
+                                    <td class="text-secondary">peason title agensy</td>
+                                    <td class="text-secondary">65</td> 
+                                    <td class="text-secondary">$0</td>
+                                    <td class="text-secondary">$0 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody> 
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">114</th>
+                                    <td class="text-secondary">shelly washington</td>
+                                    <td class="text-secondary">true title</td>
+                                    <td class="text-secondary">1</td> 
+                                    <td class="text-secondary">$0</td>
+                                    <td class="text-secondary">$0 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody>
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">115</th>
+                                    <td class="text-secondary">kim boldennotarized doc, LLC</td>
+                                    <td class="text-secondary">Luxury Brand Pros</td>
+                                    <td class="text-secondary">3</td> 
+                                    <td class="text-secondary">$380</td>
+                                    <td class="text-secondary">$0 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody>
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">118</th>
+                                    <td class="text-secondary">marcus roberston</td>
+                                    <td class="text-secondary">MA roberrston consulting</td>
+                                    <td class="text-secondary">2</td> 
+                                    <td class="text-secondary">$150</td>
+                                    <td class="text-secondary">0 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody>
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">112</th>
+                                    <td class="text-secondary">heidi moscoreli</td>
+                                    <td class="text-secondary">landmark title assurance agency of NV</td>
+                                    <td class="text-secondary">3</td> 
+                                    <td class="text-secondary">$150</td>
+                                    <td class="text-secondary">$150 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody>
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">64</th>
+                                    <td class="text-secondary">lorraine velko-gardner</td>
+                                    <td class="text-secondary">premium american title</td>
+                                    <td class="text-secondary">2</td> 
+                                    <td class="text-secondary">$0</td>
+                                    <td class="text-secondary">$0 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody>
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">41</th>
+                                    <td class="text-secondary">pV</td>
+                                    <td class="text-secondary">premier american title</td>
+                                    <td class="text-secondary">3</td> 
+                                    <td class="text-secondary">$300</td>
+                                    <td class="text-secondary">$200 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody> 
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">1</th>
+                                    <td class="text-secondary">Nandha</td>
+                                    <td class="text-secondary">BooksApp</td>
+                                    <td class="text-secondary">1</td> 
+                                    <td class="text-secondary">$250</td>
+                                    <td class="text-secondary">$280 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody> 
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">2</th>
+                                    <td class="text-secondary">2NotaeizedDocs</td>
+                                    <td class="text-secondary">premier american title</td>
+                                    <td class="text-secondary">13</td> 
+                                    <td class="text-secondary">$150</td>
+                                    <td class="text-secondary">$180 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody> 
+                                <tbody>
+                                    <tr>
+                                    <th scope="row" class="text-primary">4</th>
+                                    <td class="text-secondary">Rajal</td>
+                                    <td class="text-secondary">Conneqtly</td>
+                                    <td class="text-secondary">14</td> 
+                                    <td class="text-secondary">$500</td>
+                                    <td class="text-secondary">$580 <span><i class="fa fa-address-card float-right"></i></span></td>
+                                    </tr>
+                                </tbody>
+                            </table>  
+                    <!-- table ends-->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
